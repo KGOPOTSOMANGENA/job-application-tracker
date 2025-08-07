d1ef80273ff3a2eb40ad4fc6e115f59246ac1eca
Kgopotso Mangena <kgopotsomangena42@gmail.com>
2025-08-07 18:26:31 +0200

    updated landing page to better UI

--- job-application-tracker/src/pages/Landing.tsx
+++ job-application-tracker/src/pages/Landing.tsx
@@ -3,8 +3,44 @@ import { Link } from 'react-router-dom';
 const Landing = () => {
   return (
     <div className="landing-container">
-      <h1>Welcome to Job Application Tracker</h1>
-      <p>Track your job applications easily – applied, interviewed, and rejected.</p>
+      <div className="welcome-box">
+        <h1>Welcome to Job Application Tracker</h1>
+      </div>
+
+      <p className="subtitle">
+        Track your job applications easily  applied, interviewed, and rejected.
+      </p>
+
+
+      <h2 className="how-it-works-title">HOW IT WORKS</h2>
+      <div className="how-it-works-boxes">
+        <div className="feature-box">
+          <h3>Add The</h3>
+          <ul>
+            <li>Company name</li>
+            <li>Role</li>
+            <li>Status (Applied, Interviewed, Rejected)</li>
+            <li>Date applied</li>
+            <li>
+              Extra details about the company and job (duties, requirements, etc.)
+            </li>
+          </ul>
+        </div>
+        <div className="feature-box">
+          <h3>Track Progress</h3>
+          <p>
+            View and update your applications based on the current status to stay
+            organized and focused.
+          </p>
+        </div>
+        <div className="feature-box">
+          <h3>All In One Place</h3>
+          <p>
+            A simple dashboard to access all your job applications anytime, anywhere.
+          </p>
+        </div>
+      </div>
+
       <div className="landing-buttons">
         <Link to="/register">
           <button className="primary-button">Register</button>
@@ -18,3 +54,4 @@ const Landing = () => {
 };
 
 export default Landing;
+
