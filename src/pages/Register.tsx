17bf2467f86d4dcf05680e12e1a51efbd1389fa5
Kgopotso Mangena <kgopotsomangena42@gmail.com>
2025-08-19 14:00:23 +0200

    updated port on register now using 5000 port only

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -30,7 +30,7 @@ const Register: React.FC = () => {
 
       const newUser = { name, email, password };
 
-      await fetch('http://localhost:3000/users', {
+      await fetch('http://localhost:5000/users', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(newUser),
